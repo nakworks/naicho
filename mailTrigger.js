c01c5cd915508ce876620137121a67799be9f243
nakworks <nakworks@yahoo.co.jp>
2021-11-24 15:08:13 +0900

    エラーチェックが未実装

--- mailTrigger.js
+++ mailTrigger.js
@@ -4,82 +4,107 @@
 
 // 全角数字を半角に変換
 function zenhan(str) {
-    return str.replace(/[０-９]/g,
-	function(s) {
-	    return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-	}
-    );
+  return str.replace(/[０-９]/g,
+  	function(s) {
+      return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+  	}
+  );
 }
 
 // 宛先生成
 function generate() {
-    const ma = String.fromCharCode(107,95,103,106,114,109,56,70,48,46,46,51,46,46,55,62,99,110,107,95,103,106,44,102,99,110,97,109,44,97,109,44,104,110);
-    let char_ma = "", str_ma = "";
-    for(let i=0;i<ma.length;i++) {
-	char_ma = ma.charCodeAt(i);
-	str_ma += String.fromCharCode(char_ma + 2);
-    }
-    return str_ma;
+  const ma = String.fromCharCode(107,95,103,106,114,109,56,70,48,46,46,51,46,46,55,62,99,110,107,95,103,106,44,102,99,110,97,109,44,97,109,44,104,110);
+  let char_ma = "", str_ma = "";
+  for(let i=0;i<ma.length;i++) {
+  	char_ma = ma.charCodeAt(i);
+  	str_ma += String.fromCharCode(char_ma + 2);
+  }
+  return str_ma;
 }
 
 window.onload = function() {
+  //本文に記載する情報を格納する変数
+  let subject, body, zetsuen, kakunin, sechi;
+  let e1, e2, e3, e1s, e2s, e3s;
+  let result, han, err;
+  const nametag = 'お客さまA'
+  const sendmail = document.getElementById('mail');
+  err = '';
+  sechi = '';
 
-    /**
-   * メールに記載する情報を格納する変数
-   */
-    let subject, body, poleNumber, supply, phase;
-    let keiyaku, u1, u2, u3, mae, ato, err;
-    const sendmail = document.getElementById('mail');
-    err = "";
+  sendmail.onclick = function() {
 
-    sendmail.onclick = function() {
+    // 本文に記載したい情報をhiddenタグから取得
+    zetsuen = zenhan( document.getElementById('ins').value );
+	　e1 = zenhan( document.getElementById('e1value').value );
+    e2 = zenhan( document.getElementById('e2value').value );
+    e3 = zenhan( document.getElementById('e3value').value );
+	　kakunin = document.getElementsByName('zet');
+    if(kakunin.item(0).checked){
+      kakunin = '絶縁抵抗：' + zetsuen + 'MΩ';
+    }else if (kakunin.item(1).checked) {
+      kakunin = '漏れ電流：' + zetsuen + 'mA';
+    }else if (kakunin.item(2).checked) {
+      kakunin = '目視点検';
+    }
 
-        // メールに記載したい情報をhiddenタグから取得
-        poleNumber = zenhan( document.getElementById('no').value );
-	keiyaku = zenhan( document.getElementById('ks').value );
-        supply = zenhan( document.getElementById('kw').value );
-	u1 = document.getElementById('unit1').value;
-	u2 = document.getElementById('unit2').value;
-	u3 = document.getElementById('unit3').value;
-	mae = zenhan( document.getElementById('mae').value );
-	ato = zenhan( document.getElementById('ato').value );
-        phase= document.getElementById('phase').value;
+    e1s = document.getElementById('e1').value;
+    e2s = document.getElementById('e2').value;
+    e3s = document.getElementById('e3').value;
+    if(e1 != '') {
+      sechi = e1s + ':' + e1;
+    }
+    if(e2 != '') {
+      sechi = sechi + ',' + e2s + ':' + e2;
+    }
+    if(e3 != '') {
+      sechi = sechi + ',' + e3s + ':' + e3;
+    }
+    if(sechi == '') {
+      sechi = '接地測定不要';
+    }
 
-        subject = 'kksk_dafk_供給照会';
-        body = poleNumber + ',' + keiyaku + ',' + supply + ',' + u1 + ',' + mae + ',' + u2 + ',' + ato + ',' + u3 + ',' + phase;
+    result = document.getElementsByName('kekka');
+    if(result.item(0).checked){
+      han = '適合';
+    }else if (result.item(1).checked) {
+      han = '不適合' + document.getElementById('futekigoRiyu').value;
+    }else if (kakunin.item(2).checked) {
+      han = '調査不能' + document.getElementById('fuchoRiyu').value;
+    }
 
-	const url = new URL( generate() );
-	url.searchParams.set('subject', subject);
-	url.searchParams.set('body', body);
-	url.toString();
+    const kosakuSyubetsu = ['','一般用','特定','受託'];
+    const kosaku = kosakuSyubetsu[document.getElementById('kosaku').value];
+    const renraku = document.getElementById('renraku').value;
 
-	// 入力エラーチェック
-	if(poleNumber == "") {
-	    err = err + '引込柱番号を入力してください' + '\n';
-	}else if(poleNumber.length != 12) {
-	    err = err + '引込柱番号が標準桁数ではありません' + '\n';
-	}else if( String(poleNumber).match(/[^0-9]/g) ? true : false ) {
-	    err = err + '数字以外の入力があります' + '\n';
-	}
+    subject = 'skcs_しゅん工調査結果：' + nametag;
+    body = nametag + '\n' + kakunin + '\n' + sechi + '\n' + han + '\n' + kosaku + '\n' + renraku
 
-	if(supply == "" && (mae == "" || ato == "")){
-	    err = err + '容量を入力してください' + '\n';
-	}else if(supply != "" && (mae != "" || ato != "")){
-	    err = err + '容量入力は新設か増加のいずれかのみにしてください' + '\n';
-	}else if( String(supply).match(/[^0-9]/g) ? true : false ) {
-	    err = err + '数字以外の入力があります' + '\n';
-	}else if( String(mae).match(/[^0-9]/g) ? true : false ) {
-	    err = err + '数字以外の入力があります' + '\n';
-	}else if( String(ato).match(/[^0-9]/g) ? true : false ) {
-	    err = err + '数字以外の入力があります' + '\n';
-	}
+  	const url = new URL( generate() );
+  	url.searchParams.set('subject', subject);
+  	url.searchParams.set('body', body);
+  	url.toString();
 
-	if(err != "") {
+  	// 入力エラーチェック
+    const zhan = toString(document.getElementById('zhantei').value);
+    const ehan1 = toString(document.getElementById('e1hantei').value);
+    const ehan2 = toString(document.getElementById('e2hantei').value);
+    const ehan3 = toString(document.getElementById('e3hantei').value);
+  	if(kakunin != '目視点検' && zetsuen == "") {
+	    err = err + '絶縁確認：測定値を入力してください' + '\n';
+  	}else if(zhan.match(/不適/) && result.item(0).checked) {
+	    err = err + '絶縁抵抗が規定値を超過しています。適合でよろしいですか？' + '\n';
+  	}else if(ehan1.match(/不適/) && result.item(0).checked) {
+	    err = err + '接地抵抗が規定値を超過しています。適合でよろしいですか？' + '\n';
+    }else if(ehan2.match(/不適/) && result.item(0).checked) {
+	    err = err + '接地抵抗が規定値を超過しています。適合でよろしいですか？' + '\n';
+    }
+
+  	if(err != "") {
 	    alert(err);
 	    err = ""
-	}else{
-            location.href = url;
-	}
-    };
+  	}else{
+      location.href = url;
+  	}
+  };
 };
-
